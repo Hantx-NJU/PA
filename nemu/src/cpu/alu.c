f2bfdb874a6af236c6554a3dcdcd2171f8be7335
tracer-pa-public <tracer@njuics.org>
2019-09-18 17:54:59 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:54:59 up  2:43,  1 user,  load average: 0.09, 0.11, 0.06 473d074834442142f76aa8744a9b9078e9bac098

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -81,15 +81,29 @@ uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
 	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
+//*******************ADC*************************
+
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
+/*#ifdef NEMU_REF_ALU
 	return __ref_alu_adc(src, dest, data_size);
 #else
 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
+	assert(1);
 	return 0;
-#endif
+#endif*/
+
+	uint32_t res = 0;
+        res = dest + src + CF;
+
+        set_CF_add(res, src, data_size);
+        set_PF(res);
+        set_ZF(res, data_size);
+        set_SF(res, data_size);
+        set_OF_add(res, src, dest, data_size);
+
+        return res & (0xFFFFFFFF >> (32 - data_size));
+
 }
 
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
