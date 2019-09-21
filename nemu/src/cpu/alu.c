d5056d6bfc28cb4f13bc5b7b5709ba21022ef9ff
tracer-pa-public <tracer@njuics.org>
2019-09-21 18:18:52 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:18:52 up  1:13,  1 user,  load average: 0.21, 0.11, 0.05 4771a19b6afefba0743360f5c767e2d5b910448e

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -114,9 +114,9 @@ int set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
 	if(sign(src) != sign(result))
 		return 1;
 	else
-		return = 0;
+		return 0;
 	} else {
-	return = 0;
+	return 0;
 }
 /*	if(cpu.eflags.CF == 1){
 		if(data_size == 8 && src + dest ==0xff)
