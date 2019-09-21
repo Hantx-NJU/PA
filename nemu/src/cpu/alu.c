174a720f5773d55085a29a3abdf1ce3d021417c6
tracer-pa-public <tracer@njuics.org>
2019-09-21 18:17:18 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:17:18 up  1:11,  1 user,  load average: 0.02, 0.08, 0.03 51f93102dd0f9336eba62975af79c37397154e8

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -94,7 +94,7 @@ void set_CF_adc(uint32_t result, uint32_t src, size_t data_size)
 		cpu.eflags.CF = result < src;
 }
 
-void set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
+int set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
 {
 	switch(data_size)
        	{
@@ -112,13 +112,13 @@ void set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
 }
 	if(sign(src) == sign(dest)) {
 	if(sign(src) != sign(result))
-		cpu.eflags.OF = 1;
+		return 1;
 	else
-		cpu.eflags.OF = 0;
+		return = 0;
 	} else {
-	cpu.eflags.OF = 0;
+	return = 0;
 }
-	if(cpu.eflags.CF == 1){
+/*	if(cpu.eflags.CF == 1){
 		if(data_size == 8 && src + dest ==0xff)
 			cpu.eflags.OF = 1;
 		else if(data_size == 16 && src + dest ==0xffff)
@@ -126,6 +126,7 @@ void set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
 		else if(data_size == 32 && src + dest ==0xffffffff)
 			cpu.eflags.OF = 1;
 	}
+*/
 }
 
 
@@ -146,7 +147,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
         set_PF(res);
         set_ZF(res, data_size);
         set_SF(res, data_size);
-        set_OF_adc(res, src, dest, data_size);
+        cpu.eflags.OF = set_OF_adc(res, src, dest, data_size) || set_OF_adc(src + cpu.eflags.CF, src,cpu.eflags.CF , data_size) || set_OF_adc(dest + cpu.eflags.CF, cpu.eflags.CF, dest, data_size);
 
         return res & (0xFFFFFFFF >> (32 - data_size));
 
