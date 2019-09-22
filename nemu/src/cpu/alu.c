d49184c2305169d481f961f44bf3047eb6de8d2d
tracer-pa-public <tracer@njuics.org>
2019-09-22 22:48:26 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:48:26 up  2:27,  1 user,  load average: 0.00, 0.01, 0.00 78afa9349659fd92c4fda69255c87c24c6a5339

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -375,15 +375,29 @@ uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
+//*******************SHR*******************
+//
+void set_CF_shr(uint32_t src, uint32_t dest, size_t data_size)
+{
+	cpu.eflags.CF = (dest >> (src - 1)) & 0x1;
+	//cpu.eflags.CF = sign(dest << (data_size - src));
+}
+
 uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_shr(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+	uint32_t res = 0;
+        res = dest;
+	for(int i = 0;i < src; ++i)
+	{
+		res = (res >> 1) | (res | (0x1 << data_size))
+	}	
+	set_CF_shr(src, dest, data_size);
+	set_PF(res);
+        set_ZF(res, data_size);
+        set_SF(res, data_size);
+
+	return res & (0xFFFFFFFF >> (32 - data_size));
+
 }
 
 uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
