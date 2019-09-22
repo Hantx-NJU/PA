1a2194bff4c5a6d3a107cf16d628b2ce9a161fa6
tracer-pa-public <tracer@njuics.org>
2019-09-22 16:30:04 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:30:04 up  2:16,  1 user,  load average: 0.12, 0.10, 0.08 7f5987d4429908a0a55a4ea91578f14afd5ccdc7

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -328,15 +328,22 @@ uint32_t alu_xor(uint32_t src, uint32_t dest, size_t data_size)
 #endif
 }
 
+//********************OR***********************
+
 uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_or(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+	uint32_t res = 0;
+        res = src | dest;
+
+	cpu.eflags.CF = 0;
+	cpu.eflags.OF = 0;
+	set_PF(res);
+        set_ZF(res, data_size);
+        set_SF(res, data_size);
+
+	return res & (0xFFFFFFFF >> (32 - data_size));
+
+
 }
 
 uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
