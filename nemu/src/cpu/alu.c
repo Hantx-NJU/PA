0ed31dee0c1e33d9709df755acdd87b2c73d0cee
tracer-pa-public <tracer@njuics.org>
2019-09-22 20:27:01 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:27:01 up 6 min,  1 user,  load average: 0.08, 0.18, 0.09 b560527e13c17015bc7a181cb7d568675b3d3769

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -356,9 +356,7 @@ uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
 
 void set_CF_shl(uint32_t src, uint32_t dest, size_t data_size)
 {
-	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	uint temp = dest << (src - 1);
-	temp = sign_ext(temp & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	uint temp = dest << (32 - data_size);
 	cpu.eflags.CF = sign(temp);
 	//cpu.eflags.CF = sign(dest << (data_size - src));
 }
