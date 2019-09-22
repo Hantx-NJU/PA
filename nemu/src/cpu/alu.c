81a82fbd5987f6f4576187f1a2f3a53647fe91b2
tracer-pa-public <tracer@njuics.org>
2019-09-22 15:47:22 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:47:22 up  1:33,  1 user,  load average: 0.08, 0.03, 0.01 41b79394f5e9a8a1501dc5a8d6ebe6bf544f9d4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -134,9 +134,9 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
 	uint32_t res = 0;
         res = dest + src + cpu.eflags.CF;
-	if(cpu.eflags.CF == 1)
-		cpu.eflags.OF = set_OF_adc(src + dest, src, dest, data_size) || set_OF_adc(res, src + dest , 1, data_size);
-	else
+//	if(cpu.eflags.CF == 1)
+//		cpu.eflags.OF = set_OF_adc(src + dest, src, dest, data_size) || set_OF_adc(res, src + dest , 1, data_size);
+//	else
 		cpu.eflags.OF =	set_OF_adc(res, src, dest, data_size);
         set_CF_adc(res, src, data_size);
         set_PF(res);
