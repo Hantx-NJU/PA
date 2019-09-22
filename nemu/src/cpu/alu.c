f1cc8259a5c37f7fcbd82b461a25e1e325da798a
tracer-pa-public <tracer@njuics.org>
2019-09-22 16:45:22 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:45:22 up  2:31,  1 user,  load average: 0.09, 0.07, 0.07 723a56cdfd1ed7ea5baafd3c2a3430611a5a5b10

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -352,15 +352,24 @@ uint32_t alu_or(uint32_t src, uint32_t dest, size_t data_size)
 
 }
 
+//*********************SHL*********************
+
+void set_CF_shl(uint32_t src, uint32_t dest, size_t data_size)
+{
+	dest = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.CF = sign(dest << (src - 1));
+}
 uint32_t alu_shl(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_shl(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+	uint32_t res = 0;
+        res = dest << src;
+	
+	set_CF_shl(src, dest, data_size);
+	set_PF(res);
+        set_ZF(res, data_size);
+        set_SF(res, data_size);
+
+	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
 uint32_t alu_shr(uint32_t src, uint32_t dest, size_t data_size)
