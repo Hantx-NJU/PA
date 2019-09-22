cd39a2a8033e14acdc9fce2a57af99b0b780bca6
tracer-pa-public <tracer@njuics.org>
2019-09-22 23:27:06 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:27:06 up  3:06,  1 user,  load average: 0.22, 0.11, 0.03 cd95d7cc36258b2d539603b4a4a777073808b6ec

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -422,7 +422,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	uint32_t res = 0;
 	res = sign_ext(dest & (0xFFFFFFFF >> (32 - data_size)), data_size);
         //res = dest & (0xFFFFFFFF >> (32 - data_size));
-	for(int i = 0;i < src; ++i)
+	//for(int i = 0;i < src; ++i)
 		/*res = res >> 1;
 		switch(data_size)
        		{
