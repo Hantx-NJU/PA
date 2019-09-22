998d37faf65b0b62f8d1193d4f8580445f856b06
tracer-pa-public <tracer@njuics.org>
2019-09-22 16:12:27 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  16:12:27 up  1:58,  1 user,  load average: 0.13, 0.17, 0.06 d8d1fc3464b79d3caf4d80da7053b3a066d9e4b4

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -151,7 +151,7 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 
 void set_CF_sub(uint32_t src, uint32_t dest)
 {
-	cpu.eflags.CF =  src  > dest;
+	cpu.eflags.CF =  (src  > dest);
 	
 }
 
