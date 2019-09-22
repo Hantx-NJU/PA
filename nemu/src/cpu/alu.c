990c4316bd224576c5c4c98e31b5f0bb384ec927
tracer-pa-public <tracer@njuics.org>
2019-09-22 15:31:20 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:31:20 up  1:17,  1 user,  load average: 0.16, 0.06, 0.01 b6def0839cb326d64d5d6348fb6355a846ae2aa5

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -132,26 +132,16 @@ int set_OF_adc(uint32_t result, uint32_t src, uint32_t dest, size_t data_size)
 
 uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 {
-/*#ifdef NEMU_REF_ALU
-	return __ref_alu_adc(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(1);
-	return 0;
-#endif*/
-
 	uint32_t res = 0;
         res = dest + src + cpu.eflags.CF;
 	if(cpu.eflags.CF == 1)
 		cpu.eflags.OF = set_OF_adc(src + dest, src, dest, data_size) || set_OF_adc(res, src + dest , 1, data_size);
-	//cpu.eflags.OF = (set_OF_adc(res, src + 1, dest, data_size) || set_OF_adc(src + 1, src, 1 , data_size) || set_OF_adc(src + dest, src, dest, data_size) || set_OF_adc(res, dest + cpu.eflags.CF, src, data_size) || set_OF_adc(dest + cpu.eflags.CF, dest,cpu.eflags.CF , data_size));
 	else
 		set_OF_add((res, src, dest, data_size);
         set_CF_adc(res, src, data_size);
         set_PF(res);
         set_ZF(res, data_size);
         set_SF(res, data_size);
-        //cpu.eflags.OF = (set_OF_adc(res, src + cpu.eflags.CF, dest, data_size) || set_OF_adc(src + cpu.eflags.CF, src,cpu.eflags.CF , data_size) || (set_OF_adc(src + dest, src, dest, data_size) || set_OF_adc(res, dest + cpu.eflags.CF, src, data_size) || set_OF_adc(dest + cpu.eflags.CF, dest,cpu.eflags.CF , data_size));
 
         return res & (0xFFFFFFFF >> (32 - data_size));
 
