2f5aa80e392a3e8a395e396696e5edc51bd95527
tracer-pa-public <tracer@njuics.org>
2019-09-23 15:26:42 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:26:42 up  1:22,  1 user,  load average: 0.23, 0.09, 0.06 b790657ec2bc20ab376de9201791a6913d91efe8

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -306,13 +306,11 @@ int64_t alu_imul(int32_t src, int32_t dest, size_t data_size)
 // need to implement alu_mod before testing
 uint32_t alu_div(uint64_t src, uint64_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_div(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+	if(src == 0)
+		exit(-1);
+	else{}
+	uint32_t res = dest / src;
+	return res;
 }
 
 // need to implement alu_imod before testing
@@ -320,7 +318,7 @@ int32_t alu_idiv(int64_t src, int64_t dest, size_t data_size)
 {
 #ifdef NEMU_REF_ALU
 	return __ref_alu_idiv(src, dest, data_size);
-#else
+#elseiii
 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
 	assert(0);
 	return 0;
