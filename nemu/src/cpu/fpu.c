039d0ea1a8598c8290a47af6eb65473bbb632f46
tracer-pa-public <tracer@njuics.org>
2019-09-25 22:54:44 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:54:44 up 11 min,  1 user,  load average: 0.07, 0.14, 0.14 43cd437cd8239216523150f8df65cca78b2ce7fd

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -151,9 +151,11 @@ uint32_t internal_float_add(uint32_t b, uint32_t a)
 	uint32_t shift = 0;
 
 	/* TODO: shift = ? */
-	printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
-	assert(0);
-	assert(shift >= 0);
+//	printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
+//	assert(0);
+//	assert(shift >= 0);
+
+	shift = (fb.exponent == 0 ? fb.exponent + 1 : fb.exponent) - (fa.exponent == 0 ? fa.exponent + 1 : fa.exponent);
 
 	sig_a = (sig_a << 3); // guard, round, sticky
 	sig_b = (sig_b << 3);
