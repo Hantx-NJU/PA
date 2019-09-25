96a1e7c0f458ec8d04b47fb4ef1a4210889e35b7
tracer-pa-public <tracer@njuics.org>
2019-09-25 23:05:25 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:05:25 up 22 min,  1 user,  load average: 0.05, 0.06, 0.08 3725894be6dfe79a89da9cf49e261d6c69489163

--- nemu/src/cpu/fpu.c
+++ nemu/src/cpu/fpu.c
@@ -24,8 +24,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			/* TODO: shift right, pay attention to sticky bit*/
 			//printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
 			//assert(0);
-			uint f_sticky = sig_grs & 0x1;
-			sig_grs >> 1;
+			uint32_t f_sticky = sig_grs & 0x1;
+			sig_grs = sig_grs >> 1;
 			++exp;
 			sig_grs = sig_grs | f_sticky;
 		}
@@ -36,8 +36,8 @@ inline uint32_t internal_normalize(uint32_t sign, int32_t exp, uint64_t sig_grs)
 			//printf("\e[0;31mPlease implement me at fpu.c\e[0m\n");
 			//assert(0);
 			//overflow = true;
-			if(sign)	return n_inf;
-			else	return p_inf;
+			if(sign)	return n_inf.val;
+			else	return p_inf.val;
 		}
 		if (exp == 0)
 		{
