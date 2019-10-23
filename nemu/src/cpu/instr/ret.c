4c72660e8cc9a3c6f555eaf00063e487d19ad65a
tracer-pa-public <tracer@njuics.org>
2019-10-23 10:58:28 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  10:58:27 up 48 min,  1 user,  load average: 0.27, 0.10, 0.06 a4e4fd728b8f61d74ed6d8d71063598afcb2280c

---
+++ nemu/src/cpu/instr/ret.c
@@ -0,0 +1,15 @@
+#include "cpu/instr.h"
+
+make_instr_func(ret_near)
+{
+	//EIP <- POP()
+	OPERAND temp;
+	temp.type = OPR_MEM;
+	temp.data_size = data_size;
+	temp.addr = cpu.esp;
+	operand_read(&temp);
+	cpu.esp += data_size/8;
+	temp.addr = cpu.eip;
+	operand_write(&temp);
+	return 1+data_size/8;
+}
