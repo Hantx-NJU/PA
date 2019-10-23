e62e6fab1603770515d854df17023aed7c854289
monitor <tracer@njuics.org>
2019-10-23 10:30:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,37 +1,23 @@
 #include "cpu/instr.h"
 
-/*make_instr_func(call_near)
-{
-		OPERAND temp;
-		temp.val=cpu.ip;
-		cpu.esp-=data_size / 8;
-		temp.addr=cpu.esp;
-		temp.data_size=data_size;
-		temp.type=OPR_MEM;
-		operand_write(&temp);
-		operand_read(&opr_src);
-		cpu.eip=(cpu.eip + opr_src.val)&0x0000FFFF;
-		return 1+data_size/8;
-}*/
 make_instr_func(call_near)
-{	OPERAND rel;
-    rel.type=OPR_IMM;
-    rel.sreg=SREG_CS;
-    rel.data_size=data_size;
-    rel.addr=eip+1;
-    operand_read(&rel);
+{
+	//PUSH(EIP)
+	OPERAND rel;
+	rel.type = OPR_IMM;
+	rel.addr = cpu.eip + 1;
+	rel.data_size = data_size;
+	operand_read(&rel);
+	cpu.esp -= data_size / 8;
 
-    cpu.gpr[4].val-=data_size/8;
-    OPERAND mem;
-    mem.type=OPR_MEM;
-    mem.data_size=data_size;
-    mem.val=cpu.eip+1+data_size/8;
-    mem.sreg=SREG_SS;
-    mem.addr=cpu.gpr[4].val;
-    operand_write(&mem);
-    int offset =sign_ext(rel.val,data_size);
+	temp.val=cpu.ip;
+	cpu.esp-=data_size / 8;
+	temp.addr=cpu.esp;
+	temp.data_size=data_size;
+	temp.type=OPR_MEM;
+	operand_write(&temp);
+	operand_read(&opr_src);
+	cpu.eip=(cpu.eip + opr_src.val)&0x0000FFFF;
+	return 1+data_size/8;
+}
 
-    print_asm_1("call","",1+data_size/8,&rel);
-    cpu.eip+=offset;
-    return 1+data_size/8;
-    }
