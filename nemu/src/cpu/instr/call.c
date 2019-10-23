adb1db0b26b387b6550d30c9f3688138bd67c2e0
tracer-pa-public <tracer@njuics.org>
2019-10-23 10:18:01 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  10:18:00 up 8 min,  1 user,  load average: 0.14, 0.22, 0.14 368c73c62984d8f9463d1515d056b3fddc409d4e

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,6 +1,6 @@
 #include "cpu/instr.h"
 
-make_instr_func(call_near)
+/*make_instr_func(call_near)
 {
 		OPERAND temp;
 		temp.val=cpu.ip;
@@ -12,4 +12,26 @@ make_instr_func(call_near)
 		operand_read(&opr_src);
 		cpu.eip=(cpu.eip + opr_src.val)&0x0000FFFF;
 		return 1+data_size/8;
-}
+}*/
+make_instr_func(call_near)
+{	OPERAND rel;
+    rel.type=OPR_IMM;
+    rel.sreg=SREG_CS;
+    rel.data_size=data_size;
+    rel.addr=eip+1;
+    operand_read(&rel);
+
+    cpu.gpr[4].val-=data_size/8;
+    OPERAND mem;
+    mem.type=OPR_MEM;
+    mem.data_size=data_size;
+    mem.val=cpu.eip+1+data_size/8;
+    mem.sreg=SREG_SS;
+    mem.addr=cpu.gpr[4].val;
+    operand_write(&mem);
+    int offset =sign_ext(rel.val,data_size);
+
+    print_asm_1("call","",1+data_size/8,&rel);
+    cpu.eip+=offset;
+    return 1+data_size/8;
+    }
