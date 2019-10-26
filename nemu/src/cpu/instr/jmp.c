1788aa0fb0d84b65b55a53bcf265401b3a3a0f78
tracer-pa-public <tracer@njuics.org>
2019-10-26 20:00:59 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:00:58 up  3:34,  1 user,  load average: 0.19, 0.20, 0.11 1b294c6d33f29fd2e20f52d723f3e6cd207e0362

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -24,18 +24,18 @@ make_instr_func(jmp_short)
         OPERAND rel;
         rel.type = OPR_IMM;
         rel.sreg = SREG_CS;
-        rel.data_size = data_size;
+        rel.data_size = 8;
         rel.addr = eip + 1;
 
         operand_read(&rel);
 
         int offset = sign_ext(rel.val, data_size);
         // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+        print_asm_1("jmp", "", 1 + 8 / 8, &rel);
 
         cpu.eip += offset;
 
-        return 1 + data_size / 8;
+        return 1 + 8 / 8;
 }
 
 
