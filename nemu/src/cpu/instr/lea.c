5db393d8cc00bc297179e9aef52f452b08cbacec
tracer-pa-public <tracer@njuics.org>
2019-10-26 20:19:48 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:19:48 up  3:52,  1 user,  load average: 0.15, 0.10, 0.09 8cee0bcc438e05dc893a75472d721f9f10acf93

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -4,7 +4,7 @@ make_instr_func(lea)
 {
 	operand_read(&opr_src);
 	operand_read(&opr_dest);
-	opr_dest.val = opr_src.val;
+	opr_dest.val = opr_src.addr;
 	operand_write(&opr_dest);
 	return 1 + data_size / 8;
 }
