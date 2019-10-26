107d99b638e44aa8aa22f4d89ac0fff5a3ac5823
tracer-pa-public <tracer@njuics.org>
2019-10-26 20:49:17 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:49:17 up  4:22,  1 user,  load average: 0.00, 0.04, 0.06 dd5242c3e24f6c7ee93b57a767947a2d58edc36a

--- nemu/src/cpu/instr/lea.c
+++ nemu/src/cpu/instr/lea.c
@@ -4,10 +4,10 @@ make_instr_func(lea)
 {
 	OPERAND r, m;
 	r.data_size = data_size;
+	m.data_size = data_size;
 	int len = 1;
-	len += modrm_rm(eip + 1, &r);
-	len += modrm_rm(eip + len, &m);
-	r.val = m.val;
+	len+=modrm_r_rm(eip+1,&r,&m);
+	r.val = m.addr;
 	r.val = sign_ext(r.val, r.data_size);
 	operand_write(&r);
 	return len;
