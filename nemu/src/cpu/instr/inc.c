3df50b7a3c995aeeea75662915889c949fa486c0
tracer-pa-public <tracer@njuics.org>
2019-10-27 10:09:58 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  10:09:58 up 27 min,  1 user,  load average: 0.11, 0.10, 0.09 7fff60be3e0f89adee6a83e464cc915d2af771e6

--- nemu/src/cpu/instr/inc.c
+++ nemu/src/cpu/instr/inc.c
@@ -2,10 +2,10 @@
 
 static void instr_execute_1op()
 {
-	operand_read(&opr_dest);
+	operand_read(&opr_src);
 
-	opr_dest.val = alu_add(1, opr_dest.val, data_size);
-	operand_write(&opr_dest);
+	opr_src.val = alu_add(1, opr_src.val, data_size);
+	operand_write(&opr_src);
 }
 
 make_instr_impl_1op(inc, rm, v);
