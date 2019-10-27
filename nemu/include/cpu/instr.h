2748441680d555efef91ce4eefbed3a3a63dc7c4
tracer-pa-public <tracer@njuics.org>
2019-10-27 22:00:58 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:00:58 up  2:46,  1 user,  load average: 0.39, 0.31, 0.23 1551e3dc33d7e5bb7402a9056ccdcdf5e9d26a48

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -47,6 +47,7 @@ extern uint8_t data_size;
 #include "cpu/instr/idiv.h"
 #include "cpu/instr/mul.h"
 #include "cpu/instr/stos.h"
+#include "cpu/instr/cmps.h"
 
 
 #endif
---
+++ nemu/include/cpu/instr/cmps.h
@@ -0,0 +1,12 @@
+#ifndef __INSTR_CMPS_H__
+#define __INSTR_CMPS_H__
+
+make_instr_func(cmps_v);
+
+
+
+
+
+
+
+#endif
