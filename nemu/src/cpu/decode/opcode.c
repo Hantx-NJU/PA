dc4291c448a2d6ce0a8a4e93b0dcc342ca7205cb
tracer-pa-public <tracer@njuics.org>
2019-10-27 20:34:51 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:34:51 up  1:20,  1 user,  load average: 0.24, 0.14, 0.06 6929910d63dd78cc7ccc7b9fc7cb08942f3b1f83

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -144,7 +144,7 @@ instr_func group_x87_dd_entry[8] =
 
 /* 0xde */
 instr_func group_x87_de_entry[8] =
-    {x87_faddx, inv, inv, inv, inv, inv__ref_sub_r2rm_v, inv, inv};
+    {x87_faddx, inv, inv, inv, inv, inv, inv, inv};
 
 /* 0xdf */
 instr_func group_x87_df_entry[8] =
