0a5aa0eb485b135e238b4ca136b1fd6b584280ba
monitor <tracer@njuics.org>
2019-10-27 11:26:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -37,4 +37,6 @@ extern uint8_t data_size;
 #include "cpu/instr/and.h"
 #include "cpu/instr/adc.h"
 #include "cpu/instr/xor.h"
+#include "cpu/instr/or.h"
+
 #endif
---
+++ nemu/include/cpu/instr/or.h
@@ -0,0 +1,17 @@
+#ifndef __INSTR_XOR_H__
+#define __INSTR_XOR_H__
+
+make_instr_func(or_r2rm_v);
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
