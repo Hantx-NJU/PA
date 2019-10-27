cc7e82ff075afd557da41484059c39f0de4bedc7
monitor <tracer@njuics.org>
2019-10-27 19:27:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -88,8 +88,8 @@ void set_CF_adc(uint32_t result, uint32_t src, size_t data_size)
 {
 	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
 	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
-	if(cpu.eflags.CF == 1 && result <= src)
-		cpu.eflags.CF = 1;
+	if(cpu.eflags.CF == 1 )
+		cpu.eflags.CF = result <= src;
 	else
 		cpu.eflags.CF = result < src;
 }
