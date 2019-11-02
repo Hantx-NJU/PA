488df99608a358096c1e145d480302d21d7c2f3f
tracer-pa-public <tracer@njuics.org>
2019-11-02 09:54:09 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:54:09 up 10 min,  1 user,  load average: 0.00, 0.11, 0.09 491cb0635c02fb4b47fd47d674ef138b1c55755

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -23,3 +23,26 @@ make_instr_func(call_near)
 	return 1 + data_size / 8;
 }
 
+make_instr_func(call_near_indirect)
+{
+	//PUSH(EIP)
+	OPERAND rel, mem;
+	rel.type = OPR_IMM;
+	rel.addr = cpu.eip + 1;
+	rel.data_size = data_size;
+	operand_read(&rel);
+	cpu.esp -= data_size / 8;
+	
+	mem.type = OPR_MEM;
+	mem.data_size = data_size;
+	mem.val = cpu.eip + 1 + data_size / 8;
+	mem.addr = cpu.esp;
+	operand_write(&mem);
+
+	//EIP <- EIP + rel
+	int rel_s = sign_ext(rel.val, data_size);
+	cpu.eip = rel_s;
+
+	return 0;
+}
+
