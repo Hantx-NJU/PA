df4f719738423699686cb5bbb1a0f02ed4fbd796
tracer-pa-public <tracer@njuics.org>
2019-11-19 20:27:00 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:27:00 up  1:29,  1 user,  load average: 0.04, 0.11, 0.25 42f5f1dc3417a2ced6063044ed307f3780d90dc5

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -231,7 +231,7 @@ uint32_t expr(char *e, bool *success)
 		return 0;
 	}
 
-	for (int i = 0; i; ++i) {
+	for (int i = 0; i< nr_token; ++i) {
 		if (tokens[i].type == '-') {
 			if (i == 0)
 				tokens[i].type = NEG;
