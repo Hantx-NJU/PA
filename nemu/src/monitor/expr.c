ba2de7d1550aa29fbceb8770bff3ce95359657c2
tracer-pa-public <tracer@njuics.org>
2019-11-19 22:12:56 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:12:56 up 59 min,  1 user,  load average: 0.42, 0.37, 0.29 ef9a3fe7880adb48624d335173c830d3f682e6f6

--- nemu/src/monitor/expr.c
+++ nemu/src/monitor/expr.c
@@ -213,7 +213,7 @@ uint32_t eval(int s, int e)
 			return i;
 		}
 		else if (tokens[s].type == SYMB) {
-			bool*success;
+			bool*success=true;
 			return look_up_symtab(tokens[s].str, success);
 		}
 		
