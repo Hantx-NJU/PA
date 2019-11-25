c5730dc1ee46fd15d97e3add8eb7ab92b921a674
monitor <tracer@njuics.org>
2019-11-25 23:24:01 +0800

    git monitor commit.

--- nemu/include/memory/cache.h
+++ nemu/include/memory/cache.h
@@ -10,10 +10,9 @@ typedef struct
 }CacheLine;
 
 extern CacheLine cache[1024];   //Eight-way group connection will be implemented in the cache.c
-
-
-
-
-
+ 
+init_cache();
+uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache);
+void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache);
 
 #endif
