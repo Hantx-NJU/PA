c148889743a3e68cb7e85914993cd02037d06933
tracer-pa-public <tracer@njuics.org>
2019-11-26 21:34:15 +0800

    >  clean 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:34:15 up 12 min,  1 user,  load average: 0.56, 0.39, 0.28 f80e7015542f49df7c702b28667897694ca25eba

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -45,16 +45,16 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache)
 				//now hit
 				if(flag_cr == true)
 				{
-					//memcpy(&res, cache[group*8 + i].data + block_addr, len - suf_len);
+					memcpy(&res, cache[group*8 + i].data + block_addr, len - suf_len);
 					//res = (res << (8*suf_len)) + suf;
-					//res = res + (suf << (8*(len-suf_len)));
-					memcpy(&res, hw_mem + paddr, len);
+					res = res + (suf << (8*(len-suf_len)));
+					//memcpy(&res, hw_mem + paddr, len);
 					return res;
 				}
 				else{
-					//memcpy(&res,cache[group*8 + i].data + block_addr, len);
+					memcpy(&res,cache[group*8 + i].data + block_addr, len);
 					//return res;
-					memcpy(&res, hw_mem + paddr, len);
+					//memcpy(&res, hw_mem + paddr, len);
 					return res;
 				}
 			}
