26e11443dc953e66bac57b5f328eab78c0c801da
tracer-pa-public <tracer@njuics.org>
2019-11-26 19:39:47 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:39:47 up 51 min,  1 user,  load average: 0.63, 0.56, 0.46 96b1ef0414c31d8720a261c487d1f6fcd8efee8c

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -46,7 +46,8 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache)
 				if(flag_cr)
 				{
 					memcpy(&res, cache[group*8 + i].data + block_addr, len - suf_len);
-					res = (res << (8*suf_len)) + suf;
+					//res = (res << (8*suf_len)) + suf;
+					res = res + (suf << (8*(len-suf_len)));
 					return res;
 				}
 				else{
