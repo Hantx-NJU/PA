8b14f61e5ee3a90115adae8862698184d06c03eb
tracer-pa-public <tracer@njuics.org>
2019-11-26 23:20:50 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:20:50 up  1:58,  1 user,  load average: 0.61, 0.54, 0.42 c73baa7bb2403d675a281c5eda84b7aedfd012c9

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -1,134 +1,94 @@
+#include"memory/cache.h"
+#include "nemu.h"
 #include "memory/memory.h"
-#include "memory/cache.h"
-
-CacheLine cache[1024];
-
-void init_cache(){
-	for (int i = 0; i < 1024; ++i) {
-		cache[i].valid = false;
-	}
-}
-
-
-
-uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache)
+#include <memory.h>
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void init_cache()
 {
-	static uint32_t seed = 0;
-	++seed;
-	if(seed > 100000)	seed = 0;
-
-	uint32_t res = 0, suf = 0;
-	//memcpy(&res, hw_mem + paddr, len);
-	//return res;
-
-	uint32_t tag = paddr & 0xffffe000;
-	uint32_t group = paddr & 0x1fc0;
-	group >>= 6;
-	uint32_t block_addr = (paddr & 0x3f);
-	uint32_t blockline = -1;
-
-	int suf_len = len + block_addr - 64;
-
-	//bool flag = false;	//if hit then set flag true
-
-	//Judge if hit
-	for(int i = 0; i < 8; ++i)
-	{
-		if(cache[group * 8 + i].valid == true){
-			if(cache[group * 8 + i].sign == tag){
-				//now hit
-				if(block_addr + len > 64)
-				{
-					memcpy(&res, (cache[group*8 + i].data + block_addr), len - suf_len);
-					suf = cache_read(paddr + 64 - block_addr, block_addr + len - 64, cache);
-					res = res + (suf << (8*(64 - block_addr)));
-					return res;
-				}
-				else{
-					memcpy(&res,cache[group*8 + i].data + block_addr, len);
-					return res;
-				}
-			}
-		}
-	}
-	
-	//find empty block to load new data
-	for(int i = 0; i < 8; ++i)
-	{
-		if(cache[group*8 + i].valid == false)
-		{
-			blockline = i;
-			break;
-		}
-	}
-//memcpy(&res, hw_mem + paddr, len);
-//	return res;
-	//now we must replace one block to load new--->blockline = seed % 8
-	if(blockline == -1)	blockline = seed % 8;
-	cache[group*8 + blockline].valid = true;
-	cache[group*8 + blockline].sign = tag;
-	memcpy(cache[group*8 + blockline].data, hw_mem + (paddr&0xffffffc0), 64);
-	//memcpy(&res,cache[group*8 + blockline].data + block_addr, len);
-	memcpy(&res, hw_mem + paddr, len);
-	return res;
+    for(int i=0;i<1024;i++)
+    {
+       
+            L1_dcache[i].valid=0;
+        
+    }
 }
+uint32_t cache_read(paddr_t paddr,size_t len, CacheLine* cache)
+{
+    uint32_t group_number=(paddr>>6)&0x7f;
+    uint32_t sign_number=(paddr>>13)&0x7ffff;
+    uint32_t offset_number=paddr&0x3f ;
+    uint32_t rst;
+    for(int i=0;i<8;i++)
+    {
+        if(cache[group_number*8+i].valid==1
+           &&cache[group_number*8+i].sign==sign_number)
+        {
+            if(offset_number+len<=64)
+            {
+                memcpy(&rst,cache[group_number*8+i].block+offset_number,len);
+                return rst;
 
+            }
+            else
+            {
+                uint32_t tmp1=0,tmp2=0;
+                memcpy(&tmp1,cache[group_number*8+i].block+offset_number,64-offset_number);
+                tmp2=cache_read(paddr-offset_number+64,len+offset_number-64,cache);
+                rst=tmp1|(tmp2<<(64-offset_number)*8);
+                return rst;
+            }
+            
+        }
+    }
+
+    //miss!
+    bool have_empty=false;
+    memcpy(&rst,hw_mem+paddr,len);
+    int j=0;
+    for(;j<8;j++)
+    {
+        if(cache[group_number*8+j].valid==0)
+        {
+            have_empty=true;
+            break;
+        }
+    }
+
+    if(have_empty==false) 
+    {
+        srand((unsigned)time(0));
+        j=rand()%8;
+    }
+    cache[group_number*8+j].valid=1;
+    cache[group_number*8+j].sign=sign_number;
+    memcpy(cache[group_number*8+j].block,hw_mem+paddr-offset_number,64);
+    
+    return rst;
+    
+
+}
 void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache)
 {
-	uint32_t tag = paddr & 0xffffe000;
-	uint32_t group = paddr & 0x1fc0;
-	group >>= 6;
-	uint32_t block_addr = paddr & 0x3f;
+	for(int j=0;j<len;++j){
+		uint32_t tag = paddr & 0xffffe000;
+		uint32_t group = paddr & 0x1fc0;
+		group >>= 6;
+		uint32_t block_addr = paddr & 0x3f;
 
-	int suf_len = len + block_addr - 64,read_len = len - suf_len;
-	bool flag_cr = false;
-	//Cross Row
-	if((paddr&0xffffffc0)!=((paddr+len)&0xffffffc0)){
-		flag_cr =true;
-		if(len == 2)
+		//write through
+		for(int i = 0; i < 8; ++i)
 		{
-			cache_write(paddr + 1, 1, data & 0xf, cache);
-			len = 1;
-			data >>= 8;
-		}
-		else if(len == 4)
-		{
-			if(read_len == 1)
-			{
-				cache_write(paddr + read_len, suf_len, data & 0xfff, cache);
-				len = read_len;
-				data >>= 24;
-			}
-			else if(read_len == 2)
-			{
-				cache_write(paddr + read_len, suf_len, data & 0xff, cache);
-				len = read_len;
-				data >>= 16;
-			}
-			else if(read_len == 3)
-			{
-				cache_write(paddr + read_len, suf_len, data & 0xf, cache);
-				len = read_len;
-				data >>= 8;
-			}
-		}
-	}
-
-	//write through
-	for(int i = 0; i < 8; ++i)
-	{
-		if(cache[group * 8 + i].valid){
-			if(cache[group * 8 + i].sign == tag){
-				if(flag_cr == true)
-				{
-					memcpy(cache[group*8 + i].data + block_addr, &data, len);
-					memcpy(hw_mem + paddr, &data, len);
-				}
-				else{
-					memcpy(cache[group*8 + i].data + block_addr, &data, len);
-					memcpy(hw_mem + paddr, &data, len);
+			if(cache[group * 8 + i].valid){
+				if(cache[group * 8 + i].sign == tag){
+					uint32_t temp = data & 0xff;
+					memcpy(cache[group*8 + i].data + block_addr, &temp, 1);
+					memcpy(hw_mem + paddr, &temp, 1);
+					data >>= 8;
 				}
 			}
 		}
+		++paddr;
 	}
 }
