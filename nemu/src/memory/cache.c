be2f24d45113418a23968d93be101dc832157655
monitor <tracer@njuics.org>
2019-11-26 18:49:05 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -142,7 +142,6 @@ void cache_write(paddr_t paddr, size_t len, uint32_t data, CacheLine * cache)
 			if(cache[group * 8 + i].sign == tag){
 				memcpy(cache[group*8 + i].data + block_addr, &data, len);
 				memcpy(hw_mem + paddr, &data, len);
-				
 			}
 		}
 	}
