7b6e8f91bf25a150801bbd713dd5237d4326af6a
tracer-pa-public <tracer@njuics.org>
2019-11-27 08:19:55 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  08:19:55 up 9 min,  1 user,  load average: 0.53, 0.49, 0.35 9c7c97b9be02ebe9c94969910b28caa232df3c

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -21,8 +21,8 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache)
 	uint32_t res = 0;
 
 	//uint32_t tag = paddr & 0xffffe000;
-	uint32_t tag = (paddr>>13)&0x7f;
-	uint32_t group = (paddr>>6) & 0x1fc0;
+	uint32_t tag = (paddr>>13)&0x7ffff;
+	uint32_t group = (paddr>>6) & 0x7f;
 	uint32_t block_addr = (paddr & 0x3f);
 	int blockline = -1;
 
