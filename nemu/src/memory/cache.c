fd98a5916b935519c721479d03911e3f9322221f
monitor <tracer@njuics.org>
2019-11-27 08:23:02 +0800

    git monitor commit.

--- nemu/src/memory/cache.c
+++ nemu/src/memory/cache.c
@@ -71,25 +71,28 @@ uint32_t cache_read(paddr_t paddr, size_t len, CacheLine * cache)
 
 void cache_write(paddr_t paddr,size_t len,uint32_t data , CacheLine* cache)
 {
-   uint32_t tag = (paddr>>13)&0x7ffff;
+   	uint32_t tag = (paddr>>13)&0x7ffff;
 	uint32_t group = (paddr>>6) & 0x7f;
 	uint32_t block_addr = (paddr & 0x3f);
     memcpy(hw_mem + paddr,&data,len);
-    for(int i=0;i<8;i++)
-    {
-        if(cache[group_number*8+i].valid==1
-           &&cache[group_number*8+i].sign==sign_number)
-        {
-            if(offset_number+len<=64)
-            {
-                memcpy(cache[group_number*8+i].data+offset_number,&data,len);
-            }
-            else
-            {
-                cache_write(paddr,64-offset_number,data,cache);
-                cache_write(paddr-offset_number+64,len-64+offset_number,data>>(8*(64-offset_number)),cache);
-            }
-            break;
-        }
-    }
+   for(int i = 0; i < 8; ++i)
+	{
+		if(cache[group * 8 + i].valid == 1){
+			if(cache[group * 8 + i].sign == tag){
+				//now hit
+				if(block_addr + len > 64)
+				{
+					uint32_t suf = 0;
+					memcpy(&res, (cache[group*8 + i].data + block_addr), 64-block_addr);
+					suf = cache_read(paddr + 64 - block_addr, block_addr + len - 64, cache);
+					res = res + (suf << (8*(64 - block_addr)));
+					return res;
+				}
+				else{
+					memcpy(&res,cache[group*8 + i].data + block_addr, len);
+					return res;
+				}
+			}
+		}
+	}
 }
