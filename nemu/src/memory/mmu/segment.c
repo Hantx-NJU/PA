9a01afff2e58e02741a305815c31242919d0d543
tracer-pa-public <tracer@njuics.org>
2019-12-01 09:39:52 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:39:52 up 25 min,  1 user,  load average: 0.16, 0.28, 0.19 efd575966f0d35e215522816c0609f43826151fe

--- nemu/src/memory/mmu/segment.c
+++ nemu/src/memory/mmu/segment.c
@@ -7,7 +7,9 @@ uint32_t segment_translate(uint32_t offset, uint8_t sreg)
 	/* TODO: perform segment translation from virtual address to linear address
 	 * by reading the invisible part of the segment register 'sreg'
 	 */
-	return 0;
+
+	return cpu.segReg[sreg].base + offset;
+	//return 0;
 }
 
 // load the invisible part of a segment register
