5560c89fd523a92076b0ea8b7911446528d586ae
tracer-pa-public <tracer@njuics.org>
2019-12-03 19:02:39 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:02:38 up 17 min,  1 user,  load average: 0.87, 0.66, 0.48 4c2ff7092381ea2041af0b26db56788cd636d7fb

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -96,3 +96,18 @@ make_instr_func(mov_rm2s_w) {
 	load_sreg(sreg.addr);
 	return len;
 }
+
+make_instr_func(mov_c2r_l) {
+	int len = 1;
+	OPERAND c, r;
+	c.type = OPR_CREG;
+	r.type = OPR_REG;
+	c.data_size = 32;
+	r.data_size = 32;
+
+	len += modrm_r_rm(eip + 1, &c ,&r);
+	operand_read(&c);
+	r.val = c.val;
+	operand_write(&r);
+	return len;
+}
