356e0472c60dbdc460b5dc40b9dabf195610999d
tracer-pa-public <tracer@njuics.org>
2019-12-04 15:43:33 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:43:33 up  1:43,  1 user,  load average: 0.93, 0.48, 0.30 5e2ae7ed75896702b6067ac5a429d7bc4d6b2cc1

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,13 +9,13 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 
-	assert(PDE.present == 1);
-
 	uint32_t pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12);
 	uint32_t pde = paddr_read(pde_addr, 4);
+	assert((pde&0x1) == 1);
+
 	uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
 	uint32_t pte = paddr_read(pte_addr, 4);
-	assert(PDE.present == 1);
+	assert((pte&0x1) == 1);
 
 	return offset + (pte & 0xfffff000);
 	//printf("\nPlease implement page_translate()\n");
