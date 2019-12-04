e2a3aa0bc05363a30d0f8ae863578e1eba33f312
tracer-pa-public <tracer@njuics.org>
2019-12-04 15:51:25 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  15:51:25 up  1:51,  1 user,  load average: 1.34, 0.77, 0.48 116aafceb58482bfc45ae99a5500b2711247da04

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -59,7 +59,14 @@ uint32_t laddr_read(laddr_t laddr, size_t len)
 
 void laddr_write(laddr_t laddr, size_t len, uint32_t data)
 {
-	paddr_write(laddr, len, data);
+	assert(len == 1 || len == 2 || len == 4);
+	#ifndef IA32_PAGE
+		paddr_write(laddr, len, data);
+	#else
+		paddr_t paddr;
+		paddr = page_translate(laddr);
+		paddr_write(paddr, len, data);
+	#endif
 }
 
 uint32_t vaddr_read(vaddr_t vaddr, uint8_t sreg, size_t len)
