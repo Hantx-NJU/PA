e7532eda613a7c865788fbaebe8d9696a156df6e
tracer-pa-public <tracer@njuics.org>
2019-12-05 23:37:18 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:37:18 up 45 min,  1 user,  load average: 0.44, 0.41, 0.31 bdcea66cdfaa34a5e091e40671d9088c3e859855

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -8,11 +8,9 @@ paddr_t page_translate(laddr_t laddr)
 	//uint32_t dir = (laddr >> 22) & 0x3ff;
 	//uint32_t page = (laddr >> 12) & 0x3ff;
 	//uint32_t offset = laddr & 0xfff;
-	uint32_t dir = (laddr & 0xffc00000) >> 20;
-	uint32_t page = (laddr & 0x003ff000) >> 10;
-	uint32_t offset = (laddr & 0x00000fff);
+	
 
-	uint32_t pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12);
+	/*uint32_t pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12);
 	uint32_t pde = paddr_read(pde_addr, 4);
 	assert((pde&0x1) == 1);
 
@@ -20,7 +18,20 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t pte = paddr_read(pte_addr, 4);
 	assert((pte&0x1) == 1);
 
-	return (offset + (pte & 0xfffff000));
+	return (offset + (pte & 0xfffff000));*/
+	uint32_t dir4 = (laddr & 0xffc00000) >> 20;
+	uint32_t page4 = (laddr & 0x003ff000) >> 10;
+	uint32_t offset = (laddr & 0x00000fff);
+	uint32_t pde1 = (cpu.cr3.PDBR << 12) + dir4;
+	pde1 = paddr_read(pde1, 4);
+	if ((pde1 & 1) == 0)
+		assert(0);
+	uint32_t pte1 = (pde1 & 0xfffff000) + page4;
+	pte1 = paddr_read(pte1, 4);
+	if ((pte1 & 1) == 0)
+		assert(0);
+	uint32_t padd = (pte1 & 0xfffff000) + offset;
+	return padd;
 	//printf("\nPlease implement page_translate()\n");
 	//assert(0);
 #else
