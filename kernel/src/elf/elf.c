46a26a4fc57255dcd301f528ca56df55a6394f50
tracer-pa-public <tracer@njuics.org>
2019-12-05 23:30:55 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:30:55 up 38 min,  1 user,  load average: 0.75, 0.39, 0.28 fa026b7039098b462b7dbe96fa81846279fa60fc

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -39,15 +39,16 @@ uint32_t loader()
 
 			// remove this panic!!!
 			//panic("Please implement the loader");
-#ifndef IA32_PAGE
-/* TODO: copy the segment from the ELF file to its proper memory area */
+#ifdef IA32_PAGE
+Elf32_Addr addr = mm_malloc(ph->p_vaddr, ph->p_memsz);
+	memcpy((void*)addr, (void*)ph->p_offset, ph->p_filesz);
+	memset((void*)(addr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
+
+#else
+	/* TODO: copy the segment from the ELF file to its proper memory area */
 memcpy((void*)ph->p_vaddr, (void*)ph->p_offset, ph->p_filesz);
 /* TODO: zeror the memory area [vaddr + file_sz, vaddr + mem_sz) */
 memset((void*)(ph->p_vaddr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
-#else
-	Elf32_Addr addr = mm_malloc(ph->p_vaddr, ph->p_memsz);
-	memcpy((void*)addr, (void*)ph->p_offset, ph->p_filesz);
-	memset((void*)(addr + ph->p_filesz), 0, ph->p_memsz - ph->p_filesz);
 #endif
 
 
