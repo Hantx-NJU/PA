3ea56377d25244a688493b10ae30b18df5846d72
monitor <tracer@njuics.org>
2019-12-06 11:00:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,20 +5,7 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
-uint32_t dir4 = (laddr & 0xffc00000) >> 20;
-	uint32_t page4 = (laddr & 0x003ff000) >> 10;
-	uint32_t offset = (laddr & 0x00000fff);
-	uint32_t pde1 = (cpu.cr3.pdbr << 12) + dir4;
-	pde1 = paddr_read(pde1, 4);
-	if ((pde1 & 1) == 0)
-		assert(0);
-	uint32_t pte1 = (pde1 & 0xfffff000) + page4;
-	pte1 = paddr_read(pte1, 4);
-	if ((pte1 & 1) == 0)
-		assert(0);
-	uint32_t padd = (pte1 & 0xfffff000) + offset;
-	return padd;
-	/*uint32_t dir = (laddr >> 22) & 0x3ff;
+	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 
@@ -30,7 +17,7 @@ uint32_t dir4 = (laddr & 0xffc00000) >> 20;
 	uint32_t pte = paddr_read(pte_addr, 4);
 	assert((pte&0x1) == 1);
 
-	return (offset + (pte & 0xfffff000));*/
+	return (offset + (pte & 0xfffff000));
 	//printf("\nPlease implement page_translate()\n");
 	//assert(0);
 #else
