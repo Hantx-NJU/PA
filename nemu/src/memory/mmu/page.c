7a0201b632e216beeca1458736b7e49f1f3e787e
tracer-pa-public <tracer@njuics.org>
2019-12-08 11:19:14 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:19:15 up 3 min,  1 user,  load average: 0.59, 0.65, 0.29 66904681cc367c47b99668806e7103507c578229

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -13,7 +13,8 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t pde = paddr_read(pde_addr, 4);
 	assert((pde&0x1) == 1);
 
-	uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
+	//uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
+	uint32_t pte_addr = (page << 2) + (pde << 12);
 	uint32_t pte = paddr_read(pte_addr, 4);
 	assert((pte&0x1) == 1);
 
