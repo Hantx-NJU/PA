2657707e187bb36d1c82e4c45ebb9a24ed51ae2c
tracer-pa-public <tracer@njuics.org>
2019-12-08 13:59:41 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  13:59:41 up 14 min,  1 user,  load average: 0.33, 0.44, 0.32 82aee306eddc1c37c06f1ad4be713313ba04fb9

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -15,14 +15,14 @@ paddr_t page_translate(laddr_t laddr)
 	//uint32_t pde = hw_mem_read(pde_addr, 4);
 	assert(pde->present == 1);
 
-	PTE* pte = (PTE*)((page << 2) + (pde->val << 12) + hw_mem);
+	PTE* pte = (PTE*)((page << 2) + (pde->page_frame << 12) + hw_mem);
 	//uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
 	//uint32_t pte_addr = (page << 2) + (pde << 12);
 	//uint32_t pte = paddr_read(pte_addr, 4);
 	//uint32_t pte = hw_mem_read(pte_addr, 4);
 	assert(pte->present== 1);
 
-	return (offset + (pte->val << 12));
+	return (offset + (pte->page_frame << 12));
 	//printf("\nPlease implement page_translate()\n");
 	//assert(0);
 #else
