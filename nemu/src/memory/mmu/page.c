2ee0a5cdf64712eaec2fbeda36a4b4cc293e271f
tracer-pa-public <tracer@njuics.org>
2019-12-08 14:03:45 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  14:03:45 up 18 min,  1 user,  load average: 0.46, 0.46, 0.36 127b03c2c3722ffbdf9f55b2773e322952fcfc

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,13 +9,13 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 
-	PDE* pde = (PDE*)((dir<<2) + (cpu.cr3.pdbr<<12) + hw_mem);
+	PDE* pde = (PDE*)((dir<<2) + (cpu.cr3.pdbr<<12));
 	//uint32_t pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12);
 	//uint32_t pde = paddr_read(pde_addr, 4);
 	//uint32_t pde = hw_mem_read(pde_addr, 4);
 	assert(pde->present == 1);
 
-	PTE* pte = (PTE*)((page << 2) + (pde->page_frame << 12) + hw_mem);
+	PTE* pte = (PTE*)((page << 2) + (pde->page_frame << 12));
 	//uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
 	//uint32_t pte_addr = (page << 2) + (pde << 12);
 	//uint32_t pte = paddr_read(pte_addr, 4);
