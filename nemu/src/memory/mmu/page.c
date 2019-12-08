7bb65277d0ef17f9a2bc3da19df833c6a4592a67
monitor <tracer@njuics.org>
2019-12-08 13:50:01 +0800

    git monitor commit.

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -9,19 +9,20 @@ paddr_t page_translate(laddr_t laddr)
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
 
-	PDE* pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12) + hw_mem;
+	PDE* pde = (dir<<2) + (cpu.cr3.pdbr<<12) + hw_mem;
 	//uint32_t pde_addr = (dir<<2) + (cpu.cr3.pdbr<<12);
 	//uint32_t pde = paddr_read(pde_addr, 4);
-	uint32_t pde = hw_mem_read(pde_addr, 4);
-	assert((pde&0x1) == 1);
+	//uint32_t pde = hw_mem_read(pde_addr, 4);
+	assert(pde.present == 1);
 
+	PTE* pte = (page << 2) + (pde << 12) + hw_mem;
 	//uint32_t pte_addr = (page << 2) + (pde & 0xfffff000);
-	uint32_t pte_addr = (page << 2) + (pde << 12);
+	//uint32_t pte_addr = (page << 2) + (pde << 12);
 	//uint32_t pte = paddr_read(pte_addr, 4);
-	uint32_t pte = hw_mem_read(pte_addr, 4);
-	assert((pte&0x1) == 1);
+	//uint32_t pte = hw_mem_read(pte_addr, 4);
+	assert(pte.present== 1);
 
-	return (offset + (pte & 0xfffff000));
+	return (offset + (pte.val & 0xfffff000));
 	//printf("\nPlease implement page_translate()\n");
 	//assert(0);
 #else
