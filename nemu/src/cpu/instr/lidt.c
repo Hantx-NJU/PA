793db7166a703e34572d14f4d188dbed67d2c854
tracer-pa-public <tracer@njuics.org>
2020-01-13 09:38:00 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  09:38:00 up  1:01,  1 user,  load average: 0.55, 0.23, 0.17 b9519fa3dc848bdbab249e8842d3fc8366900773

---
+++ nemu/src/cpu/instr/lidt.c
@@ -0,0 +1,30 @@
+#include "cpu/instr.h"
+
+make_instr_func(lglidtdt)
+{
+	int len = 1;
+	OPERAND src;
+
+	len += modrm_rm(eip+1, &src);
+	src.data_size = 16;
+	operand_read(&src);
+	cpu.gdtr.limit = src.val;
+
+	src.sreg = SREG_DS;
+
+	src.addr += 2;
+
+	if(data_size == 16)
+	{
+		src.data_size = 28;
+		operand_read(&src);
+		cpu.gdtr.base = src.val;
+	}
+	else
+	{
+		src.data_size = 32;
+		operand_read(&src);
+		cpu.gdtr.base = src.val;
+	}
+	return len;
+}
