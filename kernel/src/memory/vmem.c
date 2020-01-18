022f76eee2b61fc028fe785c9eded1813edb97ff
tracer-pa-public <tracer@njuics.org>
2020-01-19 00:23:14 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  00:23:14 up  9:01,  1 user,  load average: 0.41, 0.73, 0.77 f2a12f20ebb018c0808893b617665a28c951416a

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -1,12 +1,30 @@
 #include "common.h"
 #include "memory.h"
 #include <string.h>
+// #define PAGE_SIZE 4096				  //block size
+// #define NR_PAGE (PHY_MEM / PAGE_SIZE)  //whole number of the pages
+// #define NR_PDE 1024					  //number of dir of the whole
+// #define NR_PTE 1024					  //number of table in every dir
+// #define PAGE_MASK (4096 - 1)
+// #define PT_SIZE ((NR_PTE) * (PAGE_SIZE)) //whole size in byte of each dir
 
-#define VMEM_ADDR 0xa0000
-#define SCR_SIZE (320 * 200)
-#define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) // number of page tables to cover the vmem
+#define VMEM_ADDR 0xa0000	//IO ADDR begin
+#define SCR_SIZE (320 * 200) //IO addr range(byte) 64000
+typedef union {
+	struct
+	{
+		uint32_t offset : 12;
+		uint32_t page_index : 10;
+		uint32_t dic_index : 10;
+	};
+	uint32_t val;
+} addr;
+#define NR_PT ((SCR_SIZE + PT_SIZE - 1) / PT_SIZE) //same as NR_PDE
 
 PDE *get_updir();
+PTE ioptable[SCR_SIZE / PAGE_SIZE] align_to_page;
+extern CR3 ucr3;
+#define NR_IO_dir 1
 
 void create_video_mapping()
 {
@@ -16,16 +34,21 @@ void create_video_mapping()
 	 * [0xa0000, 0xa0000 + SCR_SIZE) for user program. You may define
 	 * some page tables to create this mapping.
 	 */
-	PTE uptable[1024] align_to_page;
-	PDE * pdir = (PDE*)va_to_pa(get_updir());
-	uint32_t addr = VMEM_ADDR;
-	uint32_t i;
-	for (i = 0xa0; i <= 0xaf; i++) {
-		uptable[i].val = make_pte(addr);
-		addr = addr + 0x1000;
+	addr vaddr;
+	vaddr.val = VMEM_ADDR;
+
+	int frame_index = vaddr.page_index; // a0
+	PDE *pdir = (PDE *)va_to_pa(get_updir());
+	PTE *ptable = (PTE *)va_to_pa(ioptable); //get dir location
+	// mm_malloc(vaddr.val, SCR_SIZE);
+	pdir[0].val = make_pde(ptable); //put table into dir
+	ptable = ptable + frame_index;
+	for (uint32_t index = 0; index < 20; index++)
+	{
+		ptable->val = make_pte((frame_index << 12));
+		frame_index++;
+		ptable++;
 	}
-	pdir->val = make_pde(va_to_pa(uptable));
-	//panic("please implement me");
 }
 
 void video_mapping_write_test()
@@ -33,7 +56,7 @@ void video_mapping_write_test()
 	int i;
 	uint32_t *buf = (void *)VMEM_ADDR;
 	for (i = 0; i < SCR_SIZE / 4; i++)
-	{
+	{	
 		buf[i] = i;
 	}
 }
