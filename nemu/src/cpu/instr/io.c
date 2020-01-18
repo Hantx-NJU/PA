2534c9c5c889d5d3bf717b57e892f62b6a0e7da7
tracer-pa-public <tracer@njuics.org>
2020-01-18 17:20:24 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:20:24 up  1:58,  1 user,  load average: 0.08, 0.22, 0.26 882a59ef329aa0e35886d12c5bd0a177202c4685

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -2,32 +2,13 @@
 #include "device/port_io.h"
 
 
-make_instr_func(in_b)
-{
-    int len = 1;
-    OPERAND DX, AX;
 
-    DX.addr = REG_DX;
-    DX.type = OPR_REG;
-    DX.data_size = data_size;
-
-    AX.addr = REG_AX;
-    AX.type = OPR_REG;
-    AX.data_size = 8;
-
-    operand_read(&DX);
-    // printf("dx = %x\nRead dx val = %x\n", cpu.edx, DX.val);
-    AX.val = pio_read(DX.val, DX.data_size / 8);
-    operand_write(&AX);
-
-    return len;
-}
-/*make_instr_func(in_b) {
+make_instr_func(in_b) {
     int len = 1;
 	cpu.gpr[0]._8[0] = (uint8_t)pio_read(cpu.gpr[2]._16, 1);
 	return len;
 }
-
+/*
 make_instr_func(in_v) {
     int len = 1;
 	cpu.eax = pio_read(cpu.gpr[2]._16, 4);
