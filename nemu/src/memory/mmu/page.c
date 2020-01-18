88228c5c42459011f3cc0340b6326eeadabdd7a8
tracer-pa-public <tracer@njuics.org>
2020-01-18 22:48:42 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:48:42 up  7:26,  1 user,  load average: 0.24, 0.22, 0.26 93089721b8e3ce50a77855b5d4c75009d2622001

--- nemu/src/memory/mmu/page.c
+++ nemu/src/memory/mmu/page.c
@@ -5,6 +5,24 @@
 paddr_t page_translate(laddr_t laddr)
 {
 #ifndef TLB_ENABLED
+	laddr_analyse l;
+	l.val = laddr;
+	PDE *pde_header = (void *)(hw_mem + (cpu.cr3.PDBR << 12));
+	PDE target_dic = pde_header[l.dic_index];
+	// assert(pde_header->present);
+	PTE *pte_header = (void *)(hw_mem +
+							   (target_dic.page_frame << 12));
+	PTE target_tbl = pte_header[l.page_index];
+	assert(target_tbl.present);
+	return (target_tbl.page_frame << 12) + l.offset;
+#else
+	return tlb_read(laddr) | (laddr & PAGE_MASK);
+	;
+#endif
+}
+/*paddr_t page_translate(laddr_t laddr)
+{
+#ifndef TLB_ENABLED
 	uint32_t dir = (laddr >> 22) & 0x3ff;
 	uint32_t page = (laddr >> 12) & 0x3ff;
 	uint32_t offset = laddr & 0xfff;
@@ -30,3 +48,4 @@ paddr_t page_translate(laddr_t laddr)
 	;
 #endif
 }
+*/
