4a8cbe6b74b78874814b1eb0ece9db8f749c2bfb
monitor <tracer@njuics.org>
2020-01-19 00:48:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/adc.h
+++ nemu/include/cpu/instr/adc.h
@@ -2,6 +2,7 @@
 #define __INSTR_ADC_H__
 
 make_instr_func(adc_r2rm_v);
+make_instr_func(adc_i2rm_b);
 
 
 #endif
--- nemu/include/cpu/instr/sbb.h
+++ nemu/include/cpu/instr/sbb.h
@@ -2,7 +2,7 @@
 #define __INSTR_SBB_H__
 
 make_instr_func(sbb_rm2r_v);
-
+make_instr_func(sbb_i2rm_b);
 
 
 
