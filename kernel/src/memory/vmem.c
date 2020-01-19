6a66f5607a0e27d964c402d6921e2a06665c123e
tracer-pa-public <tracer@njuics.org>
2020-01-19 11:04:18 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  11:04:18 up  2:08,  1 user,  load average: 0.72, 0.55, 0.51 fa32a86379f87f8cf67df55b4dc8f606fbbadc92

--- kernel/src/memory/vmem.c
+++ kernel/src/memory/vmem.c
@@ -20,7 +20,7 @@ void create_video_mapping()
 	PDE * pd = (PDE*)va_to_pa(get_updir());
 	uint32_t addr = VMEM_ADDR;
 	uint32_t i;
-	for (i = 0xa0; i <= 0xaf; i++,addr += 0x1000;) {
+	for (i = 0xa0; i <= 0xaf; i++,addr += 0x1000) {
 		table[i].val = make_pte(addr);
 	}
 	pd->val = make_pde(va_to_pa(table));
