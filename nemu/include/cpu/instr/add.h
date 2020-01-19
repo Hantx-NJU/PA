46d83bdef9cf57074c2d5f812591b57daf4e8fe2
tracer-pa-public <tracer@njuics.org>
2020-01-19 17:38:42 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  17:38:42 up 9 min,  1 user,  load average: 0.95, 0.65, 0.35 bbe6d839e773f58ae689933dd8b6428c63706

--- nemu/include/cpu/instr/add.h
+++ nemu/include/cpu/instr/add.h
@@ -7,6 +7,7 @@ make_instr_func(add_r2rm_v);
 make_instr_func(add_i2rm_bv);
 make_instr_func(add_i2rm_b);
 make_instr_func(add_rm2r_v);
+make_instr_func(add_rm2r_b);
 
 
 
--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -16,6 +16,7 @@ make_instr_impl_2op(add, r, rm, v);
 make_instr_impl_2op(add, i, rm, bv);
 make_instr_impl_2op(add, i, rm, b);
 make_instr_impl_2op(add, rm, r, v);
+make_instr_impl_2op(add, rm, r, b);
 
 
 
