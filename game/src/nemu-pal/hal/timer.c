3af1b6a340b253c4a794a3dbb5bda69238898f64
tracer-pa-public <tracer@njuics.org>
2020-01-19 18:09:04 +0800

    >  nemu 181860027 hantx Linux debian 4.19.0-5-686 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  18:09:04 up 39 min,  1 user,  load average: 0.55, 0.50, 0.41 af238680e2804d34218a0b5e7d40ac642d87f62e

--- game/src/nemu-pal/hal/timer.c
+++ game/src/nemu-pal/hal/timer.c
@@ -35,5 +35,7 @@ uint32_t SDL_GetTicks()
 void SDL_Delay(uint32_t ms)
 {
 	/* TODO: Return from this function after waiting for `ms' milliseconds. */
-	assert(0);
+	//assert(0);
+	uint32_t target = jiffy + (ms * HZ / 1000);
+	while (jiffy < target);
 }
